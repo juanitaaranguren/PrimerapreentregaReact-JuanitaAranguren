f537f9e4d022229c10e73a380662621d430dc71a
juana <jarangor8@gmail.com>
2023-08-13 16:00:57 -0500

    loader+

--- src/ItemListContainer.js
+++ src/ItemListContainer.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from "react";
 import ItemList from "./ItemList";
-import productosData from "./productos"; 
-import Loader from "./Loader";
-
+import productosData from "./productos";
+import { BeatLoader } from "react-spinners"; 
+import "./ItemListContainer.scss"; 
 
 function ItemListContainer(props) {
-    const [productos, setProductos] = useState([]); 
-    const [loading, setLoading] = useState (true)
+    const [productos, setProductos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-       
         setTimeout(() => {
             setProductos(productosData);
-            setLoading (false);
+            setLoading(false);
         }, 2000);
     }, []);
 
-    if (loading) {
-      return (
-        <Loader/>
-      )
-    } else {
-
-      return (
+    return (
         <main>
             <h2 className="text-[20px] text-center">{props.greeting}</h2>
-            <ItemList productos={productos} />
-        </main>)
-    }
+            {loading ? (
+                <div className="loader-container">
+                    <BeatLoader color={"#123abc"} loading={loading} size={20} />
+                </div>
+            ) : (
+                <ItemList productos={productos} />
+            )}
+        </main>
+    );
 }
+
 export default ItemListContainer;
