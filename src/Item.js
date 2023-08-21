d84dab6103497897ba35aaf66d4f150cfdb37bef
juana <jarangor8@gmail.com>
2023-08-21 17:21:47 -0500

    botones

--- src/Item.js
+++ src/Item.js
@@ -1,86 +1,53 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { cartContext } from "./CustomProvider";
+import DetalleMoto from "./DetalleMoto";
 
 function Item(props) {
+  const valorDelContexto = useContext(cartContext);
+  const [showDetalle, setShowDetalle] = useState(false);
 
-const valorDelContexto = useContext (cartContext);
+  const toggleDetalle = () => {
+    setShowDetalle(!showDetalle);
+  };
 
-return (
-<div> 
-       <main className="border p-4">
-         <article>
-               <h3 className="text-lg font-bold">{props.title}</h3>
-              <img
-                   className="w-full h-[150px] object-contain mt-2"
-                src={props.image}
-                alt=""
-               />
-              <p className="mt-2">{props.descripcion}</p>
-              <p className="mt-2">${props.precio}</p>
-               <p className="mt-2">{props.marca}</p>
-               {/* <button className="mt-2 boton-ver-detalle" onClick={toggleDetalle}>
-                Ver detalle
-               </button>
-               {showDetalle && <DetalleMoto producto={props} onClose={toggleDetalle} />} */}
-              <p>Cantidad : {valorDelContexto.cantidad}</p>
-          <button
-            onClick={() =>
-              valorDelContexto.setCantidad(valorDelContexto.cantidad + 1) // Incrementa la cantidad correctamente
-            }
-          >
-            sumar
-          </button>
-             </article>
-             
-           </main>
-</div>
-)
+  const limpiarCarrito = () => {
+    valorDelContexto.setCantidad(0); // Establece la cantidad en 0 para limpiar el carrito
+  };
+
+  return (
+    <div>
+      <main className="border p-4">
+        <article>
+          <h3 className="text-lg font-bold">{props.title}</h3>
+          <img
+            className="w-full h-[150px] object-contain mt-2"
+            src={props.image}
+            alt=""
+          />
+          <p className="mt-2">{props.descripcion}</p>
+          <p className="mt-2">${props.precio}</p>
+          <p className="mt-2">{props.marca}</p>
+          
+          <div className="space-y-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() =>
+                valorDelContexto.setCantidad(valorDelContexto.cantidad + 1)}>
+              Agregar
+            </button>
+            <button className="boton-ver-detalle" onClick={toggleDetalle}>
+              Ver detalle
+            </button>
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={limpiarCarrito}>
+              Limpiar
+            </button>
+          </div>
+          
+        </article>
+      </main>
+      {showDetalle && <DetalleMoto producto={props} onClose={toggleDetalle} />}
+    </div>
+  );
 }
+
 export default Item;
-
-
-
-// import React, { useState } from "react";
-// import DetalleMoto from "./DetalleMoto"
-
-// import DetalleMoto from "./DetalleMoto";
-
-// function Item(props) {
-//   const [showDetalle, setShowDetalle] = useState(false);
-//   const { agregarAlCarrito } = useState(); // Usa el hook para acceder al contexto
-
-//   const toggleDetalle = () => {
-//     setShowDetalle(!showDetalle);
-//   };
-
-//   const handleAgregarAlCarrito = (producto) => {
-//     agregarAlCarrito(producto); // Agrega el producto al carrito a través del contexto
-//     props.setCarritoCount((prevCount) => prevCount + 1); // Actualiza el contador en el NavBar
-//   };
-
-//   return (
-//     <main className="border p-4">
-//       <article>
-//             <h3 className="text-lg font-bold">{props.title}</h3>
-//            <img
-//                 className="w-full h-[150px] object-contain mt-2"
-//              src={props.image}
-//              alt=""
-//             />
-//            <p className="mt-2">{props.descripcion}</p>
-//            <p className="mt-2">${props.precio}</p>
-//             <p className="mt-2">{props.marca}</p>
-//             <button className="mt-2 boton-ver-detalle" onClick={toggleDetalle}>
-//              Ver detalle
-//             </button>
-//           </article>
-//            <button className="mt-2 boton-agregar-carrito" onClick={handleAgregarAlCarrito}>
-//         Agregar al carrito
-//           </button>
-//           {showDetalle && <DetalleMoto producto={props} onClose={toggleDetalle} />}
-//         </main>
-//       );
-//     }
-
-//     export default Item;
-    
